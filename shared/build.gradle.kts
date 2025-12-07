e7cd40c404c7061791a0a92e5810c932858aec22
Santiago Mattiauda <santimattius@gmail.com>
2025-12-06 23:03:58 -0300

    [feature] setup for publish

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,11 +4,15 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidKMPLibrary)
+    alias(libs.plugins.mavenPublish)
 }
+
+group = "io.github.santimattius.resilient"
+version = "1.0.0-ALPHA01"
 
 kotlin {
     androidLibrary {
-        namespace = "com.santimattius.kmp.shared"
+        namespace = "io.github.santimattius.resilient"
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
@@ -38,6 +42,41 @@ kotlin {
             implementation(libs.kotlin.test)
             implementation(libs.kotlinx.coroutines.test)
             implementation(libs.turbine)
+        }
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "resilient", version.toString())
+
+    pom {
+        name = "Resilient Kotlin Multiplatform Library"
+        description =
+            "A Kotlin Multiplatform library providing resilience patterns (Timeout, Retry, Circuit Breaker, Rate Limiter, Bulkhead, Hedging, Cache, Fallback) for suspend functions."
+        inceptionYear = "2025"
+        url = "https://github.com/santimattius/kmp-resilient/"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "santiago-mattiauda"
+                name = "Santiago Mattiauda"
+                url = "https://github.com/santimattius"
+            }
+        }
+        scm {
+            url = "https://github.com/santimattius/kmp-resilient/"
+            connection = "scm:git:git://github.com/santimattius/kmp-resilient.git"
+            developerConnection = "scm:git:ssh://git@github.com/santimattius/kmp-resilient.git"
         }
     }
 }
